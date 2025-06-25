b27c223d527573044295a4bba8ad929a3176178b
Kalle <kalle@kandru.de>
2025-06-25 14:16:41 +0200

    bump version to 1.9.0

--- src/Version.cs
+++ src/Version.cs
@@ -2,6 +2,6 @@ namespace Parachute
 {
     public partial class Parachute
     {
-        public override string ModuleVersion => "1.8.23";
+        public override string ModuleVersion => "1.9.0";
     }
 }

d274ab9356cfb239d3cb60cf61262498adbd5e20
derkalle4 <kalle@kandru.de>
2025-12-29 11:52:34 +0100

    bump version to 25.12.1

--- src/Version.cs
+++ src/Version.cs
@@ -2,6 +2,6 @@ namespace Parachute
 {
     public partial class Parachute
     {
-        public override string ModuleVersion => "25.10.1";
+        public override string ModuleVersion => "25.12.1";
     }
 }
